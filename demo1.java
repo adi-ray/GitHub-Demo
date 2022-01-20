09c889b9c822681e8e5336ed932c5048870cf3c3
adi-ray <96347576+adi-ray@users.noreply.github.com>
2022-01-20 19:05:28 +0530

    first

---
+++ demo1.java
@@ -0,0 +1,8 @@
+class demo1
+{
+    public static void main(String args[])
+    {
+        System.out.println("******               Welcome         ********");
+        System.out.println("****** GitHub Code Innovation Series ********");
+    }
+}
